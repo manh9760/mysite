6009dd1dcd986790c23c8a3b4f8db7dedff0eda9
manh9760 <manh.nguyen9760@gmail.com>
2020-12-17 23:29:32 +0700

    finished chapter 2

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
+from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -13,8 +15,15 @@ class PostListView(ListView):
 	template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
 	object_list = Post.published.all()
+	tag = None
+
+	# list all posts tagged with a specific tag
+	if tag_slug:
+		tag = get_object_or_404(Tag, slug=tag_slug)
+		object_list = object_list.filter(tags__in=[tag])
+
 	paginator = Paginator(object_list, 3) # 3 posts in each page
 	page = request.GET.get('page')
 	try:
@@ -25,11 +34,36 @@ def post_list(request):
 	except EmptyPage:
 		# If page is out of range deliver last page of results
 		posts = paginator.page(paginator.num_pages)
-	return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+	return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 def post_detail(request, post):
 	post = get_object_or_404(Post, slug=post, status='published')
-	return render(request, 'blog/post/detail.html', {'post': post})
+	# List of active comments for this post
+	comments = post.comments.filter(active=True)
+	new_comment = None
+	if request.method == 'POST':
+		# A comment was posted
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			# Create Comment object but don't save to database yet
+			new_comment = comment_form.save(commit=False)
+			# Assign the current post to the comment
+			new_comment.post = post
+			# Save the comment to the database
+			new_comment.save()
+	else:
+		comment_form = CommentForm()
+	# List of similar posts
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+	return render(request, 
+		'blog/post/detail.html', 
+		{'post': post, 
+		'comments': comments, 
+		'new_comment': new_comment, 
+		'comment_form': comment_form,
+		'similar_posts': similar_posts})
 
 def post_share(request, post_id):
 	# Retrieve post by id
